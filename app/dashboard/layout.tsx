d3ca9d36cf7b8aa245739152cce89df31bfe7ea0
viktorkasap <kasap.victor@gmail.com>
2024-11-17 15:48:11 -0700

    feat: Added experemental PPR

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,1 +1,8 @@
+/*
+    ! experimental_ppr
+    * CanaryOnlyError: The experimental feature "experimental.ppr" can only be enabled when using the latest canary version of Next.js.
+    `pnpm add next@canary`
+*/
+export const experimental_ppr = true;
+
 export { DashboardLayout as default, metadata } from '@/app/layouts/dashboard';
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    /*
+      ! experimental_ppr
+      * CanaryOnlyError: The experimental feature "experimental.ppr" can only be enabled when using the latest canary version of Next.js.
+      `pnpm add next@canary`
+    */
+    ppr: 'incremental',
+  },
 };
 
 export default nextConfig;
