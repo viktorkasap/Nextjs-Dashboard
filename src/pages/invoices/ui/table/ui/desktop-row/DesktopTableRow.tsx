bb404351725807c846a2b8706d9a1028f426d026
viktorkasap <kasap.victor@gmail.com>
2024-12-21 12:43:33 -0700

    feat: Added memo for tables row

--- src/pages/invoices/ui/table/ui/desktop-row/DesktopTableRow.tsx
+++ src/pages/invoices/ui/table/ui/desktop-row/DesktopTableRow.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ComponentType, ReactNode } from 'react';
+import { ComponentType, memo, ReactNode } from 'react';
 
 import clsx from 'clsx';
 
@@ -18,37 +18,33 @@ interface DesktopTableRowProps {
   DeleteInvoice: ComponentType<DeleteInvoiceProps>;
 }
 
-export const DesktopTableRow = ({
-  invoice,
-  renderCustomerAvatar,
-  renderStatusInvoice,
-  renderUpdateInvoice,
-  DeleteInvoice,
-}: DesktopTableRowProps) => {
-  const { isDeleting } = useDeleteInvoiceContext();
+export const DesktopTableRow = memo(
+  ({ invoice, renderCustomerAvatar, renderStatusInvoice, renderUpdateInvoice, DeleteInvoice }: DesktopTableRowProps) => {
+    const { isDeleting } = useDeleteInvoiceContext();
 
-  return (
-    <tr
-      className={clsx(
-        'w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg',
-        { 'opacity-50': isDeleting },
-      )}>
-      <td className="whitespace-nowrap py-3 pl-6 pr-3">
-        <div className="flex items-center gap-3">
-          {renderCustomerAvatar}
-          <p>{invoice.name}</p>
-        </div>
-      </td>
-      <td className="whitespace-nowrap px-3 py-3">{invoice.email}</td>
-      <td className="whitespace-nowrap px-3 py-3">{formatCurrency(invoice.amount)}</td>
-      <td className="whitespace-nowrap px-3 py-3">{formatDateToLocal(invoice.date)}</td>
-      <td className="whitespace-nowrap px-3 py-3">{renderStatusInvoice}</td>
-      <td className="whitespace-nowrap py-3 pl-6 pr-3">
-        <div className="flex justify-end gap-3">
-          {renderUpdateInvoice}
-          <DeleteInvoice id={invoice.id} />
-        </div>
-      </td>
-    </tr>
-  );
-};
+    return (
+      <tr
+        className={clsx(
+          'w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg',
+          { 'opacity-50': isDeleting },
+        )}>
+        <td className="whitespace-nowrap py-3 pl-6 pr-3">
+          <div className="flex items-center gap-3">
+            {renderCustomerAvatar}
+            <p>{invoice.name}</p>
+          </div>
+        </td>
+        <td className="whitespace-nowrap px-3 py-3">{invoice.email}</td>
+        <td className="whitespace-nowrap px-3 py-3">{formatCurrency(invoice.amount)}</td>
+        <td className="whitespace-nowrap px-3 py-3">{formatDateToLocal(invoice.date)}</td>
+        <td className="whitespace-nowrap px-3 py-3">{renderStatusInvoice}</td>
+        <td className="whitespace-nowrap py-3 pl-6 pr-3">
+          <div className="flex justify-end gap-3">
+            {renderUpdateInvoice}
+            <DeleteInvoice id={invoice.id} />
+          </div>
+        </td>
+      </tr>
+    );
+  },
+);
--- src/pages/invoices/ui/table/ui/mobile-row/MobileTableRow.tsx
+++ src/pages/invoices/ui/table/ui/mobile-row/MobileTableRow.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode, ComponentType } from 'react';
+import { ReactNode, ComponentType, memo } from 'react';
 
 import clsx from 'clsx';
 
@@ -18,37 +18,33 @@ interface MobileTableRowProps {
   DeleteInvoice: ComponentType<DeleteInvoiceProps>;
 }
 
-export const MobileTableRow = ({
-  invoice,
-  renderStatusInvoice,
-  renderCustomerAvatar,
-  renderUpdateInvoice,
-  DeleteInvoice,
-}: MobileTableRowProps) => {
-  const { isDeleting } = useDeleteInvoiceContext();
+export const MobileTableRow = memo(
+  ({ invoice, renderStatusInvoice, renderCustomerAvatar, renderUpdateInvoice, DeleteInvoice }: MobileTableRowProps) => {
+    const { isDeleting } = useDeleteInvoiceContext();
 
-  return (
-    <div className={clsx('mb-2 w-full rounded-md bg-white p-4', { 'opacity-50': isDeleting })}>
-      <div className="flex items-center justify-between border-b pb-4">
-        <div>
-          <div className="mb-2 flex items-center">
-            {renderCustomerAvatar}
-            <p>{invoice.name}</p>
+    return (
+      <div className={clsx('mb-2 w-full rounded-md bg-white p-4', { 'opacity-50': isDeleting })}>
+        <div className="flex items-center justify-between border-b pb-4">
+          <div>
+            <div className="mb-2 flex items-center">
+              {renderCustomerAvatar}
+              <p>{invoice.name}</p>
+            </div>
+            <p className="text-sm text-gray-500">{invoice.email}</p>
           </div>
-          <p className="text-sm text-gray-500">{invoice.email}</p>
+          {renderStatusInvoice}
         </div>
-        {renderStatusInvoice}
+        <div className="flex w-full items-center justify-between pt-4">
+          <div>
+            <p className="text-xl font-medium">{formatCurrency(invoice.amount)}</p>
+            <p>{formatDateToLocal(invoice.date)}</p>
+          </div>
+          <div className="flex justify-end gap-2">
+            {renderUpdateInvoice}
+            <DeleteInvoice id={invoice.id} />
+          </div>
+        </div>
       </div>
-      <div className="flex w-full items-center justify-between pt-4">
-        <div>
-          <p className="text-xl font-medium">{formatCurrency(invoice.amount)}</p>
-          <p>{formatDateToLocal(invoice.date)}</p>
-        </div>
-        <div className="flex justify-end gap-2">
-          {renderUpdateInvoice}
-          <DeleteInvoice id={invoice.id} />
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+  },
+);
